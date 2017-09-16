9d22634637eb7f27d88b51858ce9640b50155f05
naresharjun <naresharjun@gmail.com>
2017-09-16 13:14:34 +0200

    first commit map

--- www/js/app.js
+++ www/js/app.js
@@ -6,109 +6,141 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic',
-                          , 'starter.controllers'
-                          , 'starter.services'
-                          , 'starter.directives'
-                          , 'monospaced.elastic'
-                          , 'ksSwiper'
-                          , 'ionic.contrib.ui.tinderCards2'
-              ])
+  , 'starter.controllers'
+  , 'starter.services'
+  , 'starter.directives'
+  , 'monospaced.elastic'
+  , 'ksSwiper'
+  , 'ionic.contrib.ui.tinderCards2',
+  'ionic', 'ngCordova'
+])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-.config(function ($provide, $ionicConfigProvider, $compileProvider) {
-  $ionicConfigProvider.tabs.position('bottom');
-  // $ionicConfigProvider.scrolling.jsScrolling(false);
-  // $translateProvider.useStaticFilesLoader({
-  //     prefix: 'l10n/',
-  //     suffix: '.json'
-  //   });
-  // $translateProvider.preferredLanguage("en");
-  // $translateProvider.fallbackLanguage("en");
-  $ionicConfigProvider.scrolling.jsScrolling(false);
-  $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|cdvfile|file|filesystem|blob):|data:image\//);
-  $ionicConfigProvider.backButton.text(null).icon('ion-arrow-left-c color-coral');
-})
-.config(function($stateProvider, $urlRouterProvider) {
-
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
       }
-    }
-  })
-  .state('tab.dash-match', {
-    url: '/dash/:matchId',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash-match.html',
-        controller: 'MatchCtrl'
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
       }
-    }
+    });
   })
+  .config(function ($provide, $ionicConfigProvider, $compileProvider) {
+    $ionicConfigProvider.tabs.position('bottom');
+    // $ionicConfigProvider.scrolling.jsScrolling(false);
+    // $translateProvider.useStaticFilesLoader({
+    //     prefix: 'l10n/',
+    //     suffix: '.json'
+    //   });
+    // $translateProvider.preferredLanguage("en");
+    // $translateProvider.fallbackLanguage("en");
+    $ionicConfigProvider.scrolling.jsScrolling(false);
+    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|cdvfile|file|filesystem|blob):|data:image\//);
+    $ionicConfigProvider.backButton.text(null).icon('ion-arrow-left-c color-coral');
+  })
+  .config(function ($stateProvider, $urlRouterProvider) {
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
+
+      // Each tab has its own nav history stack:
+
+      .state('tab.dash', {
+        url: '/dash',
+        views: {
+          'tab-dash': {
+            templateUrl: 'templates/tab-dash.html',
+            controller: 'DashCtrl'
+          }
         }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+      })
+      .state('tab.dash-match', {
+        url: '/dash/:matchId',
+        views: {
+          'tab-dash': {
+            templateUrl: 'templates/tab-dash-match.html',
+            controller: 'MatchCtrl'
+          }
         }
-      }
-    })
+      })
+      .state('tab.maps', {
+        url: '/maps',
+        views: {
+          'tab-maps': {
+            templateUrl: 'templates/tab-maps.html',
+            controller: 'MapCtrl'
+          }
+        }
+      })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
+      .state('tab.account', {
+        url: '/account',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-account.html',
+            controller: 'AccountCtrl'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
+
+  })
+  .controller('MapCtrl', function ($scope, $state, $cordovaGeolocation) {
+    var options = {timeout: 10000, enableHighAccuracy: true};
+
+    $cordovaGeolocation.getCurrentPosition(options).then(function (position) {
+
+      var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      var mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+      //Wait until the map is loaded
+      google.maps.event.addListenerOnce($scope.map, 'idle', function () {
+
+        var marker = new google.maps.Marker({
+          map: $scope.map,
+          animation: google.maps.Animation.DROP,
+          position: latLng
+        });
+
+        var infoWindow = new google.maps.InfoWindow({
+          content: "Here I am!"
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+          infoWindow.open($scope.map, marker);
+        });
+
+      });
+
+    }, function (error) {
+      console.log("Could not get location");
+    });
   });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
 
-});
