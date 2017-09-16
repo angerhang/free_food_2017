59fe576dafa6029df9e98c55349421f3b68bd4ed
naresharjun <naresharjun@gmail.com>
2017-09-16 19:06:37 +0200

    markers added with minimum desc

--- www/js/app.js
+++ www/js/app.js
@@ -33,22 +33,6 @@ angular.module('starter', ['ionic', 'ngCordova'])
 
 })
 
-.factory('Markers', function($http) {
-
-  var markers = [];
-
-  return {
-    getMarkers: function(params){
-
-      return $http.get("http://www.example.com/markers.php",{params:params}).then(function(response){
-          markers = response;
-          return markers;
-      });
-
-    }
-  }
-
-})
 
 .factory('GoogleMaps', function($cordovaGeolocation, $ionicLoading, $rootScope, $cordovaNetwork, Markers, ConnectivityMonitor){
 
@@ -61,12 +45,12 @@ angular.module('starter', ['ionic', 'ngCordova'])
     var options = {timeout: 10000, enableHighAccuracy: true};
 
     $cordovaGeolocation.getCurrentPosition(options).then(function(position){
-      
+
       var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
       var mapOptions = {
         center: latLng,
-        zoom: 15,
+        zoom: 10,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
 
@@ -74,19 +58,37 @@ angular.module('starter', ['ionic', 'ngCordova'])
 
       //Wait until the map is loaded
       google.maps.event.addListenerOnce(map, 'idle', function(){
-        loadMarkers();
 
-        //Reload markers every time the map moves
-        google.maps.event.addListener(map, 'dragend', function(){
-          console.log("moved!");
-          loadMarkers();
-        });
+        var locations = [
+          ['Bondi Beach', -33.890542, 151.274856, 4],
+          ['Coogee Beach', -33.923036, 151.259052, 5],
+          ['Cronulla Beach', -34.028249, 151.157507, 3],
+          ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+          ['Maroubra Beach', -33.950198, 151.259302, 1],
+          ['Zurich',47.3768866,8.541694,6],
+          ['Zurich Technopark',47.389161,8.5150677,7],
+          ['Zurich Flughafen',47.4582165,8.5554755,8]
+        ];
 
-        //Reload markers every time the zoom changes
-        google.maps.event.addListener(map, 'zoom_changed', function(){
-          console.log("zoomed!");
-          loadMarkers();
-        });
+        var marker, i;
+
+        for (i = 0; i < locations.length; i++) {
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+          });
+          var infoWindowContent = "<h4>" + locations[i][0] + "</h4>";
+
+          var infowindow = new google.maps.InfoWindow({
+            content: infoWindowContent
+          });
+          google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+              infowindow.setContent(locations[i][0]);
+              infowindow.open(map, marker);
+            }
+          })(marker, i));
+        }
 
         enableMap();
 
@@ -117,7 +119,7 @@ angular.module('starter', ['ionic', 'ngCordova'])
     //This function will be called once the SDK has been loaded
     window.mapInit = function(){
       initMap();
-    };  
+    };
 
     //Create a script element to insert into the page
     var script = document.createElement("script");
@@ -141,135 +143,7 @@ angular.module('starter', ['ionic', 'ngCordova'])
       loadGoogleMaps();
     } else {
       enableMap();
-    }       
-  }
-
-  function loadMarkers(){
-
-      var center = map.getCenter();
-      var bounds = map.getBounds();
-      var zoom = map.getZoom();
-
-      //Convert objects returned by Google to be more readable
-      var centerNorm = {
-          lat: center.lat(),
-          lng: center.lng()
-      };
-
-      var boundsNorm = {
-          northeast: {
-              lat: bounds.getNorthEast().lat(),
-              lng: bounds.getNorthEast().lng()
-          },
-          southwest: {
-              lat: bounds.getSouthWest().lat(),
-              lng: bounds.getSouthWest().lng()
-          }
-      };
-
-      var boundingRadius = getBoundingRadius(centerNorm, boundsNorm);
-
-      var params = {
-        "centre": centerNorm,
-        "bounds": boundsNorm,
-        "zoom": zoom,
-        "boundingRadius": boundingRadius
-      };
-
-      var markers = Markers.getMarkers(params).then(function(markers){
-        console.log("Markers: ", markers);
-        var records = markers.data.result;
-
-        for (var i = 0; i < records.length; i++) {
-
-          var record = records[i];
-
-          // Check if the marker has already been added
-          if (!markerExists(record.lat, record.lng)) {
-              
-              var markerPos = new google.maps.LatLng(record.lng, record.lat);
-              // add the marker
-              var marker = new google.maps.Marker({
-                  map: map,
-                  animation: google.maps.Animation.DROP,
-                  position: markerPos
-              });
-
-              // Add the marker to the markerCache so we know not to add it again later
-              var markerData = {
-                lat: record.lat,
-                lng: record.lng,
-                marker: marker
-              };
-
-              markerCache.push(markerData);
-
-              var infoWindowContent = "<h4>" + record.name + "</h4>";          
-
-              addInfoWindow(marker, infoWindowContent, record);
-          }          
-
-        }
-
-      });    
-  }
-
-  function markerExists(lat, lng){
-      var exists = false;
-      var cache = markerCache;
-      for(var i = 0; i < cache.length; i++){
-        if(cache[i].lat === lat && cache[i].lng === lng){
-          exists = true;
-        }
-      }
-      
-      return exists;
-  }
-
-  function getBoundingRadius(center, bounds){
-    return getDistanceBetweenPoints(center, bounds.northeast, 'miles');    
-  }
-
-  function getDistanceBetweenPoints(pos1, pos2, units){
-
-    var earthRadius = {
-        miles: 3958.8,
-        km: 6371
-    };
-    
-    var R = earthRadius[units || 'miles'];
-    var lat1 = pos1.lat;
-    var lon1 = pos1.lng;
-    var lat2 = pos2.lat;
-    var lon2 = pos2.lng;
-    
-    var dLat = toRad((lat2 - lat1));
-    var dLon = toRad((lon2 - lon1));
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-    Math.sin(dLon / 2) *
-    Math.sin(dLon / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c;
-    
-    return d;
-
-  }
-
-  function toRad(x){
-      return x * Math.PI / 180;
-  }
-
-  function addInfoWindow(marker, message, record) {
-
-      var infoWindow = new google.maps.InfoWindow({
-          content: message
-      });
-
-      google.maps.event.addListener(marker, 'click', function () {
-          infoWindow.open(map, marker);
-      });
-      
+    }
   }
 
   function addConnectivityListeners(){
@@ -285,18 +159,18 @@ angular.module('starter', ['ionic', 'ngCordova'])
       $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
         disableMap();
       });
-     
+
     }
     else {
 
       //Same as above but for when we are not running on a device
       window.addEventListener("online", function(e) {
         checkLoaded();
-      }, false);    
+      }, false);
 
       window.addEventListener("offline", function(e) {
         disableMap();
-      }, false);  
+      }, false);
     }
 
   }
@@ -340,7 +214,7 @@ angular.module('starter', ['ionic', 'ngCordova'])
     isOnline: function(){
 
       if(ionic.Platform.isWebView()){
-        return $cordovaNetwork.isOnline();    
+        return $cordovaNetwork.isOnline();
       } else {
         return navigator.onLine;
       }
@@ -349,7 +223,7 @@ angular.module('starter', ['ionic', 'ngCordova'])
     ifOffline: function(){
 
       if(ionic.Platform.isWebView()){
-        return !$cordovaNetwork.isOnline();    
+        return !$cordovaNetwork.isOnline();
       } else {
         return !navigator.onLine;
       }
